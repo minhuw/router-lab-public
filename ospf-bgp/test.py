861fb99aaa9e63d18e649ba2165ef4c56970f4cc
minhuw <wangmh15@gmail.com>
2023-02-18 20:54:06 +0800

    Update code and writeup for Lab III.

--- ospf-bgp/test.py
+++ ospf-bgp/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 
 NETWORK_CONFIG = {
@@ -12,10 +14,39 @@ NETWORK_CONFIG = {
 }
 
 def check_connectivity(source: str, destination: str) -> bool:
-    response = subprocess.run(["docker", "exec", source, "ping", "-I", NETWORK_CONFIG[source], NETWORK_CONFIG[destination], "-c", "4"], capture_output=True)
+    response = subprocess.run(["docker", "exec", source, "ping", "-I", NETWORK_CONFIG[source], NETWORK_CONFIG[destination], "-c", "1"], capture_output=True)
     return False if response.returncode else True
 
+def check_path(source: str, destination: str, target_path: list[str]) -> bool:
+    response = subprocess.run(["docker", "exec", source, "traceroute", NETWORK_CONFIG[destination]], capture_output=True)
+    records = response.stdout.decode("utf-8").strip().split("\n")[1:]
+    paths = [line.strip().split(" ")[2].split(".")[3] for line in records]
+    print(f"Path {source} -> {destination}: {source[1]} -> {' -> '.join(paths)}")
+    return paths == target_path
+
+
 if __name__ == "__main__":
+    done = True
     for source in NETWORK_CONFIG.keys():
+        print(f"{source} -> ", end="") 
         for destination in NETWORK_CONFIG.keys():
-            print(f"{source} -> {destination}: {check_connectivity(source, destination)}")
+            connected = check_connectivity(source, destination)
+            done &= connected
+            print(f"{destination}: {connected} ", end="")
+        print("")
+
+    if done:
+        print("Pass Connectivity Test")
+    else:
+        print("Fail Connectivity Test")
+        exit(1)
+
+    if not check_path("R1", "R3", ['5', '6', '8', '7', '3']):
+        print("Path R1 -> R3 is not correct")
+        exit(1)
+    if not check_path("R3", "R1", ['7', '6', '8', '5', '1']):
+        print("Path R3 -> R1 is not correct")
+        exit(1)
+
+    print("Pass Path Test")
+    exit(0)
