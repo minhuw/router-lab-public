734162b6dcbf02f9499769b515c52b63dfa55ca9
minhuw <wangmh15@gmail.com>
2023-02-26 14:12:13 +0800

    Add hints and submission scripts for Lab III.

---
+++ ospf-bgp/pack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+if [[ $# -eq 0  || -z $1 ]];
+then
+    echo "please provide your student ID: ./pack.sh <student_id>"
+    exit 1
+fi
+
+containerlab save -t start.clab.yaml && tar czvf "router_submit_$1.tar.gz" ./clab-topo/*/flash/startup-config
